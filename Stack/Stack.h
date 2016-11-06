4e31f0500a1db71d79c52fb67cf94e2c8e1765c7
christian otkjær <christian.otkjaer@gmail.com>
2016-11-06 16:33:20 +0100

    Initial Commit

---
+++ Stack/Stack.h
@@ -0,0 +1,19 @@
+//
+//  Stack.h
+//  Stack
+//
+//  Created by Christian Otkjær on 06/11/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stack.
+FOUNDATION_EXPORT double StackVersionNumber;
+
+//! Project version string for Stack.
+FOUNDATION_EXPORT const unsigned char StackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stack/PublicHeader.h>
+
+
